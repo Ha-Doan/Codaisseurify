7fa69b70dd2a20f4e219466b73c7c55c9a497734
Ha-Doan <doanthanhha@gmail.com>
2017-11-10 00:01:49 +0100

    Add has_many to class Artist, do not have belongs_to in class Song, write test for belongs_to.  Error: undefined method 'artist' from song.artist

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -2,11 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Song, type: :model do
   describe "association with artist" do
-    let(:artist) { create :artist }
+  let(:artist) { create :artist }
 
-    it "-belongs to an artist" do
-      song = artist.songs.build(name: "Shine, Jesus, Shine")
-      expect(song.artist).to eq(artist)
-    end
+  it "belongs to an artist" do
+  song = artist.songs.build(name: "In Christ alone")
+
+  expect(song.artist).to eq(artist)
   end
+end
 end

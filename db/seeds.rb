923bec9d55005e818ccb25360ae9cfc033b4c836
Ha-Doan <doanthanhha@gmail.com>
2017-11-11 20:42:31 +0100

    Add test. Have has_one in class Artist. Do not have belongs_to in class Photo. Error: undefined method 'artist' from 'photo.artist' for <Photo:0x...>. Also update seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 Song.destroy_all
-Artist.destroy_all
 Photo.destroy_all
+Artist.destroy_all
+
 #Create artists
 artist1 = Artist.create!({ name: "Nora", date_of_birth: Date.new(1977,10,22), nationality: "Malaysian"})
 artist2 = Artist.create!({ name: "Matthijs", date_of_birth: Date.new(1986,8,19), nationality: "Dutch"})
@@ -8,11 +9,14 @@ artist3 = Artist.create!({ name: "Trang", date_of_birth: Date.new(1980,7,17), na
 artist4 = Artist.create!({ name: "Ximena", date_of_birth: Date.new(1983,7,15), nationality: "Columbian"})
 
 #Create songs
-song1 = Song.create!({ name: "Shine, Jesus, Shine", category: "Gospel/Christian music", composer: "Harriet", artist_id: 1})
-song2 = Song.create!({ name: "Hello", category: "Pop", composer: "Ha", artist_id: 1})
-song3 = Song.create!({ name: "I saw the light", category: "Blues", composer: "Harriet", artist_id: 2})
-song4 = Song.create!({ name: "Only you", category: "Rock", composer: "Glory", artist_id: 2})
-song5 = Song.create!({ name: "Blessed be your name", category: "Gospel/Christian music", composer: "Johan", artist_id: 3})
-song6 = Song.create!({ name: "Happy new year", category: "Pop", composer: "Abba", artist_id: 3})
-song7 = Song.create!({ name: "Color of the wind", category: "Pop", composer: "David", artist_id: 4})
-song8 = Song.create!({ name: "The Gospel train is coming", category: "Gospel/Christian music", composer: "Peter", artist_id: 4})
+song1 = Song.create!({ name: "Shine, Jesus, Shine", category: "Gospel/Christian music", composer: "Harriet", artist: artist1})
+song2 = Song.create!({ name: "Hello", category: "Pop", composer: "Ha", artist: artist1})
+song3 = Song.create!({ name: "I saw the light", category: "Blues", composer: "Harriet", artist: artist2})
+song4 = Song.create!({ name: "Only you", category: "Rock", composer: "Glory", artist: artist2})
+song5 = Song.create!({ name: "Blessed be your name", category: "Gospel/Christian music", composer: "Johan", artist: artist3})
+song6 = Song.create!({ name: "Happy new year", category: "Pop", composer: "Abba", artist: artist3})
+song7 = Song.create!({ name: "Color of the wind", category: "Pop", composer: "David", artist: artist4})
+song8 = Song.create!({ name: "The Gospel train is coming", category: "Gospel/Christian music", composer: "Peter", artist: artist4})
+
+#Photo
+photo1 = Photo.create!(artist: artist1, remote_image_url: "http://res.cloudinary.com/hacloudinary/image/upload/v1510427017/tulip1_utdy90.jpg")

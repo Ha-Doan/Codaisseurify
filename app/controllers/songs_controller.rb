34d40e6b9906468270901ac1a3e53c0e42720a7b
Ha-Doan <doanthanhha@gmail.com>
2017-11-12 18:34:29 +0100

    Create a new song for artist through 'form_for with nested resources'.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,16 +1,30 @@
 class SongsController < ApplicationController
-before_action :set_song, only: [:show, :new, :create]
+before_action :set_song, only: [:show, :delete]
 
 def index
   @artist= Artist.find(params[:artist_id])
 end
 def show
+end
 
+def new
+  @artist= Artist.find(params[:artist_id])
+  @song = @artist.songs.build
+end
+def create
+  @artist= Artist.find(params[:artist_id])
+  @song = @artist.songs.build(song_params)
+
+  if @song.save
+    redirect_to @artist, notice: "Song created."
+  else
+    render :new
+  end
 end
 private
 
 def set_song
-    @song = Song.find(params[:id])
+    @song = Song.find(params[:artist_id])
 end
 
 def song_params
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,6 @@
 class Artist < ApplicationRecord
+
   has_one :photo, dependent: :destroy
   has_many :songs, dependent: :destroy
+  accepts_nested_attributes_for :songs
 end

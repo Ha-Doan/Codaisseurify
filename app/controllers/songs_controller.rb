f914332db8f76f450f7f03fa493df9b093b7304c
Ha-Doan <doanthanhha@gmail.com>
2017-11-12 15:53:37 +0100

    Create the index page for songs. Show the list of songs of a specific artist.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,21 @@
 class SongsController < ApplicationController
-  def index
-    @songs = Song.all
-  end
+before_action :set_song, only: [:show, :new, :create]
+
+def index
+  @artist= Artist.find(params[:artist_id])
+end
+def show
+
+end
+private
+
+def set_song
+    @song = Song.find(params[:id])
+end
+
+def song_params
+  params
+    .require(:song)
+    .permit(:name, :category, :composer)
+end
 end
